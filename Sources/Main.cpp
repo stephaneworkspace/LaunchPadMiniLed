fb316cee6928b98accc9f2dc850716d123172c6c
Stéphane Bressani <stephane@bressani.dev>
2023-08-13 17:08:05 +0200

    la led fonctionne

--- Sources/Main.cpp
+++ Sources/Main.cpp
@@ -16,6 +16,51 @@ std::atomic<bool> done(false);
 // Gestionnaire de signal pour CTRL+C
 void signalHandler(int signum) { done = true; }
 
+int main() {
+    RtMidiOut *midiout = 0;
+
+    try {
+        midiout = new RtMidiOut();
+    } catch (RtMidiError &error) {
+        error.printMessage();
+        return 0;
+    }
+
+    // Cherchez le Launchpad Mini parmi les ports disponibles.
+    unsigned int ports = midiout->getPortCount();
+    int launchpadPort = -1;
+    for (unsigned int i = 0; i < ports; i++) {
+        std::string portName = midiout->getPortName(i);
+        if (portName.find("Launchpad Mini") != std::string::npos) {
+            launchpadPort = i;
+            break;
+        }
+    }
+
+    if (launchpadPort == -1) {
+        std::cout << "Launchpad Mini non trouvé!" << std::endl;
+        delete midiout;
+        return 0;
+    }
+
+    // Ouvrez le port MIDI pour le Launchpad Mini.
+    midiout->openPort(launchpadPort);
+
+    // Envoyez un message pour allumer une LED.
+    // Comme dans l'exemple Python, nous utilisons une note avec une vélocité.
+    // Vous devrez peut-être ajuster le numéro de note et la vélocité.
+    std::vector<unsigned char> message;
+    message.push_back(144); // Note on
+    message.push_back(60);  // Note number
+    message.push_back(127); // Velocity
+    midiout->sendMessage(&message);
+
+    delete midiout;
+    return 0;
+}
+
+
+/*
 int main()
 {
     // Pour de l'appareil
@@ -40,6 +85,7 @@ std::endl; return 1;
 
     return 0;
 }
+ */
 /*
 int main()
 {
